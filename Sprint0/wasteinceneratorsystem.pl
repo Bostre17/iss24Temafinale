f4d00f25f36a6765631e717e444ea67f8e9bac53
Lorenzo Severini <161623047+lorenzo-severini@users.noreply.github.com>
2025-03-23 21:58:39 +0100

    Project

---
+++ Sprint0/wasteinceneratorsystem.pl
@@ -0,0 +1,8 @@
+%====================================================================================
+% wasteinceneratorsystem description   
+%====================================================================================
+event( burnEnd, burnEnd(BTIME) ).
+%====================================================================================
+context(ctxincinerator, "localhost",  "TCP", "8001").
+ qactor( incenerator, ctxincinerator, "it.unibo.incenerator.Incenerator").
+ static(incenerator).
